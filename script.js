b2d9286235b1ba4b3bcc6fde68cb9fa5ece9c259
Anusmita Mukherjee <mukherjeeanusmita0@gmail.com>
2022-07-24 23:32:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$(() => {
+
+     const socket = io();
+     socket.on('message', addMessage);
+   
+     $('#send').click(() => {
+   
+       const name = $('#name').val().trim();
+       const message = $('#message').val().trim();
+   
+       if (name == "" || message == "")
+         alert("empty fields are not allowed");
+       else {
+         sendMessage({
+           name: name,
+           text: message
+         });
+       }
+   
+       $('#name').val("");
+       $('#message').val("");
+     });
+   
+     getMessages();
+   });
+   
+   function addMessage(message) {
+     $('#messages').prepend(`
+         <h4> ${message.name} </h4>
+         <p> ${message.text} </p>
+     `);
+   }
+   
+   function getMessages() {
+     $.get('http://localhost:3000/messages', (data) => {
+       data.forEach(addMessage);
+     });
+   }
+   
+   function sendMessage(message) {
+     $.post('http://localhost:3000/messages', message);
+   }
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+io.on('connection', () => {
+  console.log('a user is connected');
+});
+
+// mongodb atlas connection and server creation
+const dbUrl = 'mongodb+srv://anusmita:root@project.rekphi1.mongodb.net/?retryWrites=true&w=majority';
+
+let server;
+
+mongoose.connect(dbUrl, (err) => {
+
+  if (err) {
+      console.log("unable to connect to db :(");
+      console.log("Exiting...");
+      console.log("err:", err);
+  }
+  else {
+      console.log("db connection established...");
+
+      // create server and listen on port
+      server = http.listen(3000, (err) => {
+        if (err) console.log("unable to listen on port...") && console.log("err: ", err);
+        else console.log("server is running on port", server.address().port);
+      });
+  }
+});
+
+// defining schema and compiling model
+const Message = mongoose.model('Message', {
+  name: String,
+  text: String
+});
+
+
+// ROUTES
+
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    if (err)
+      res.sendStatus(500);
+    else
+      res.send(messages);
+  });
+});
+
+app.post('/messages', (req, res) => {
+
+  if (req.body.name.trim() == "" || req.body.text.trim() == "")
+    res.sendStatus(500);
+
+  let message = new Message(req.body);
+
+  message.save(err => {
+
+    if (!err) {
+      io.emit('message', req.body);
+      res.sendStatus(200);
+    }
+    else
+      res.sendStatus(500);
+  });
+});
